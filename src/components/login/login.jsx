468f6f7297f0457e950a986bebfe38cd177e27eb
lisaye1999 <lisaye1999@gmail.com>
2020-10-25 21:56:26 -0400

    Created file for login component and register component. Set up login component.

---
+++ src/components/login/login.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import './login.css'
+
+class Login extends Component {
+    // state = {  }
+
+
+    render() { 
+        return(
+        <div className="base-container">
+            <div className="content">
+                <div className="form">
+                    <form>
+                        <div className="form-group">
+                            <label htmlFor="username">Username</label>
+                            <input type="text" name="username" placeholder="username" className="form-control"/>
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="password">Password</label>
+                            <input type="text" name="password" placeholder="password" className="form-control"/>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <div className="footer">
+                <button type="button" className="btn btn-primary">Login</button>
+            </div>
+
+        </div>
+        ); // return
+    }
+}
+ 
+export default Login;
